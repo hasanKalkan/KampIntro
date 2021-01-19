4e24162e451b1416f659d8bb9856e2ffa516682d
krack16 <47774247+krack16@users.noreply.github.com>
2021-01-19 12:09:30 +0300

    guncelle

---
+++ RefAndOut/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RefAndOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int refDeger = 5;//başlangıç değeri atanması zorunludur.
+            int outDeger;//başlangıç değeri atanması zorunlu değildir.
+
+            ChangeMethod(out outDeger, ref refDeger);
+
+            Console.WriteLine("ChangeMethod'dan sonra refDeger : " + refDeger); // 7
+            Console.WriteLine("ChangeMethod'dan sonra outDeger: " + outDeger); // 8
+        }
+
+        static void ChangeMethod(out int i, ref int j)
+        {
+            i = 8; // i argümanına bir değer atamak zorunludur
+            j = j + 2; // j için böyle bir zorunluluk yoktur
+        }
+    }
+}
