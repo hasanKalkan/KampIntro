0ebe04907381a13ac9b97b61136629f53f9d14c7
krack16 <47774247+krack16@users.noreply.github.com>
2021-01-19 09:33:36 +0300

    güncelle

--- ClassMetotDemo/Musteri.cs
+++ ClassMetotDemo/Musteri.cs
@@ -6,5 +6,9 @@ namespace ClassMetotDemo
 {
     class Musteri
     {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+
     }
 }
