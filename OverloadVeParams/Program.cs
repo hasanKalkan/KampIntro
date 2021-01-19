587b0bc95026d75a338fbc4dd0b5337fe781f8dc
krack16 <47774247+krack16@users.noreply.github.com>
2021-01-19 17:08:23 +0300

    güncelle

---
+++ OverloadVeParams/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace OverloadVeParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(topla(2, 5));
+            Console.WriteLine(topla(2, 5,9));
+            Console.WriteLine(toplaParams(2, 5,9,4,5,6));
+        }
+
+        static int topla(int sayi1,int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        static int topla(int sayi1, int sayi2,int sayi3)
+        {
+            return sayi1 + sayi2 + sayi3 ;
+        }
+
+        static int toplaParams(params int[] sayilar)
+        {
+            return sayilar.Sum();
+        }
+    }
+}
