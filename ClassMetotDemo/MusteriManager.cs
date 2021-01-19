b3988cee0419078c55b0e5cc67b6563108473302
krack16 <47774247+krack16@users.noreply.github.com>
2021-01-19 09:48:34 +0300

    güncelle

--- ClassMetotDemo/MusteriManager.cs
+++ ClassMetotDemo/MusteriManager.cs
@@ -6,5 +6,15 @@ namespace ClassMetotDemo
 {
     class MusteriManager
     {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " isimli müşteri başarıyla eklendi.");
+        }
+
+        public void Sil(string musteriAdi, string musteriSoyadi)
+        {
+            Console.WriteLine(musteriAdi + " isimli müşteri başarıyla silindi.");
+        }
     }
+
 }
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,30 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Ahmet";
+            musteri1.Soyad = "Can";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Veli";
+            musteri2.Soyad = "Kaya";
+
+            MusteriManager musteriManager = new MusteriManager();
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+            foreach(var musteri in musteriler)
+            {
+                Console.Write("Müşteri adı: " + musteri.Ad);
+                Console.Write(", soyadı: " + musteri.Soyad+"\n");
+                musteriManager.Ekle(musteri);
+            }
+
+            Console.WriteLine("\n----------------Metotlar----------------");
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+            Console.WriteLine("\n");
+            musteriManager.Sil("Kamil", "Can");
+            musteriManager.Sil("Ayşe", "Nur");
         }
     }
 }
