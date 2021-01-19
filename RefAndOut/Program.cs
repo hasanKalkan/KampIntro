577602e612ee28c54a8c4f6066e0c203bede87ba
krack16 <47774247+krack16@users.noreply.github.com>
2021-01-19 16:05:49 +0300

    guncelle

--- RefAndOut/Program.cs
+++ RefAndOut/Program.cs
@@ -2,23 +2,48 @@
 
 namespace RefAndOut
 {
+   
     class Program
     {
-        static void Main(string[] args)
+
+        static void Main()
         {
-            int refDeger = 5;//başlangıç değeri atanması zorunludur.
-            int outDeger;//başlangıç değeri atanması zorunlu değildir.
 
-            ChangeMethod(out outDeger, ref refDeger);
+            int sayi1 = 3; //ref olduğu için değer atamamız gerekiyor.
+            Console.WriteLine("Başlangıç değeri: {0}", sayi1); //Başlangıç değeri: 3
 
-            Console.WriteLine("ChangeMethod'dan sonra refDeger : " + refDeger); // 7
-            Console.WriteLine("ChangeMethod'dan sonra outDeger: " + outDeger); // 8
+            Kare(sayi1); //ref ve out olmadan metot kullandık.
+            Console.WriteLine("Kare() metodu sonrası: {0}", sayi1); //Kare() metodu sonrası: 3
+
+            RefKare(ref sayi1); //RefKare metodunu çağırarak sayının değerini değiştirdik.
+            Console.WriteLine("RefKare() metodu sonrası: {0}", sayi1); //RefKare() metodu sonrası: 9
+
+            RefKare(ref sayi1); //RefKare metodunu çağırarak sayının değerini yine değiştirdik.
+            Console.WriteLine("2.RefKare() metodu sonrası: {0}", sayi1); //2.RefKare() metodu sonrası: 81
+
+            int sayi2;  //OutKare için sayi2 tanımladık ama değer atamadık..
+            OutKare(out sayi2); //OutKare metodunu çağırarak sayının değerini değiştirdik.
+            Console.WriteLine("OutKare() sonrası: " + sayi2); //OutKare() metodu sonrası: 25
+
+            OutKare(out sayi2);
+            Console.WriteLine("2.OutKare() sonrası: " + sayi2); //2.OutKare() metodu sonrası: 25 değişmedi.
+
         }
 
-        static void ChangeMethod(out int i, ref int j)
+        static void Kare(int sayi1)
         {
-            i = 8; // i argümanına bir değer atamak zorunludur
-            j = j + 2; // j için böyle bir zorunluluk yoktur
+            sayi1 = sayi1 * sayi1;
+        }
+
+        static void RefKare(ref int sayi1)
+        {
+            sayi1 = sayi1 * sayi1;
+        }
+
+        static void OutKare(out int sayi2)
+        {
+            sayi2=5;
+            sayi2 = sayi2 * sayi2;
         }
     }
 }
