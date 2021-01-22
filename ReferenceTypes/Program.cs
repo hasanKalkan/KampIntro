e4e8a0e7e85bbecb642214569c33952d8249eb9b
krack16 <47774247+krack16@users.noreply.github.com>
2021-01-22 23:14:15 +0300

    aa

--- ReferenceTypes/Program.cs
+++ ReferenceTypes/Program.cs
@@ -13,7 +13,7 @@ namespace ReferenceTypes
             sayi2 = 100;
             Console.WriteLine("sayı1: "+sayi1);
 
-
+            //referans tipler-heap
             int[] sayilar1 = new int[] { 1, 2, 3 };
             int[] sayilar2 = new int[] { 10, 20, 30 };
 
@@ -23,8 +23,26 @@ namespace ReferenceTypes
 
             foreach(int sayi in sayilar1)
             {
-                Console.WriteLine(sayi);
+                Console.WriteLine("sayilar1: "+sayi);
             }
         }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+
+    }
+
+    class Customer:Person //Inheritance:Kaltım-Miras
+    {
+        public string CreditCardNumber { get; set; }
+    }
+
+    class Employee:Person //Person özellikleri employee miras bırakılır
+    {
+        public int EmployeeNumber { get; set; }
     }
 }
