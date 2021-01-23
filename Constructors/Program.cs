a029f8831a3cdc6f12282bb6c5ce5990fa2ab313
krack16 <47774247+krack16@users.noreply.github.com>
2021-01-23 16:55:38 +0300

    güncelle

--- Constructors/Program.cs
+++ Constructors/Program.cs
@@ -6,15 +6,31 @@ namespace Constructors
     {
         static void Main(string[] args)
         {
-            Customer customer = new Customer();
+            Customer customer1 = new Customer() { Id = 1, FirstName = "Ali", LastName = "Can", City = "Bursa" };
+            Customer customer2 = new Customer(2, "Ayşe", "Can", "Bursa");
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.FirstName = "Ahmet";
+            customer3.LastName = "Can";
+            customer3.City = "İstanbul";
+            Console.WriteLine(customer1.FirstName);
         }
     }
 
     class Customer
     {
-        public Customer()
+        public Customer() //Constructor-yapıcı blok
         {
             Console.WriteLine("Constructor çalıştırıldı.");
+        }
+
+        public Customer(int id,string firstName,string lastName,string city) //Constructor-yapıcı blok - overloading yapıldı.
+        {
+            Console.WriteLine("Constructor çalıştırıldı.");
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
         }
 
         public int Id { get; set; }
