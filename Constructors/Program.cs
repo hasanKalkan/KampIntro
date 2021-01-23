38f115873d4b4453d93c5b8541f4d6b9b71db14f
krack16 <47774247+krack16@users.noreply.github.com>
2021-01-23 16:46:01 +0300

    güncelle

---
+++ Constructors/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+        }
+    }
+
+    class Customer
+    {
+        public Customer()
+        {
+            Console.WriteLine("Constructor çalıştırıldı.");
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+    }
+}
