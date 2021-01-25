fdb833273a47aa502e5cfc69337b2b32f9270d01
krack16 <47774247+krack16@users.noreply.github.com>
2021-01-25 08:49:01 +0300

    ekle

---
+++ Classes/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    class Customer
+    {
+        //field tanımlama - bu özellik pek kullanılmaz.
+        //public string FirstName; //class içinde özellik tanımlanacaksa aşağıdaki property özelliği kullanılır. 
+
+        //propperty tanımlama
+        public int Id { get; set; }
+        private string _firstName;
+        public string FirstName { 
+            get { return "Mrs. "+_firstName; }  //değerin başına Mrs ekledik
+            set { _firstName = value; } 
+        }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer = new Customer();
+            customer.City = "Bursa";
+            customer.Id = 1;
+            customer.FirstName = "Ali";
+            customer.LastName = "Can";
+
+            Customer customer2 = new Customer //Customer sınıfı set bloğu çalışır.
+            {
+                Id = 2,
+                City = "Bursa",
+                FirstName = "Ayşe",
+                LastName = "Can"
+            };
+
+            Console.WriteLine(customer2.FirstName); //Customer sınıfı get bloğu çalışır
+        }
+    }
+
+    
+    
+}
+
