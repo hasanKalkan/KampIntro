7103b93e664b4fa3ac2854d25f63fef0f64b6606
krack16 <47774247+krack16@users.noreply.github.com>
2021-01-26 08:58:00 +0300

    260121

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -8,6 +8,12 @@ namespace Inheritance
 {
     class Program
     {
+
+
+        /// <summary>
+        /// abstract sınıflar da inheritance kullanılır. 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Person[] persons = new Person[3]
@@ -44,13 +50,22 @@ namespace Inheritance
 
     }
 
-    class Customer : Person
+    class Person2
+    {
+
+    }
+
+    class Customer : Person // ,Person2 yazamayız. interface gibi iki özellik yazamayız.
     {
         public string City { get; set; }
     }
 
-    class Student : Person
+    class Student : Person //yanına istediğimiz kadar interface yazabiliriz. ,IPerson, IPerson2 gibi. önce inheritance sonra interface yazılır. 
     {
         public string Department { get; set; }
     }
+
+   //class ların tek başına bir anlamı vardır. ama interface lerin tek başına bir anlamı yoktur.
+   // ikisi de inheritance gibi miraslama yapabiliyor. 
+   // aslında interface ler de bir implemantasyondur.
 }
