31643d68a09327ef5513c7dae1e14106c2efebb3
krack16 <47774247+krack16@users.noreply.github.com>
2021-01-26 08:57:43 +0300

    260121

---
+++ Inheritance/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Customer
+                {
+                    FirstName="Ali" 
+                },
+                new Student
+                {
+                    FirstName="Ayşe"
+                },
+                new Person
+                {
+                    FirstName="Ahmet"
+                }
+            };
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastNanme { get; set; }
+
+    }
+
+    class Customer : Person
+    {
+        public string City { get; set; }
+    }
+
+    class Student : Person
+    {
+        public string Department { get; set; }
+    }
+}
