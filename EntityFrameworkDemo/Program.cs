1f4690158c45ad57e8f7ea2a53a3ea437683d413
krack16 <47774247+krack16@users.noreply.github.com>
2021-02-04 21:49:21 +0300

    4221

---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
